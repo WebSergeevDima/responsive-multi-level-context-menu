185a4bc8f48bf269d6401e05b32493c1f1ce4e91
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-07-12 19:45:42 +0300

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const nav = `
+    const navHTML = `
     <ul>
         <li><a href="#" class="js-context-menu__link">Меню 1</a></li>
         <li><a href="#" class="js-context-menu__link">Меню 2</a></li>
@@ -52,116 +52,100 @@ document.addEventListener('DOMContentLoaded', () => {
 </ul>
     `;
 
-    document.querySelector('#contextBlock').addEventListener('contextmenu', (e) => {
-
-        const body = document.querySelector('body');
-        const contextMenu = document.createElement('nav');
-
-        const config = {
-            contextMenuWidth: 150,
-            windowInnerWidth: 0,
-            windowInnerHeight: 0
-        }
-
-        contextMenu.classList.add('js-context-menu');
-
-        const getWindowInners = () => {
-            const windowInnerWidth = window.innerWidth;
-            const windowInnerHeight = window.innerHeight;
-
-            config.windowInnerWidth = windowInnerWidth;
-            config.windowInnerHeight = windowInnerHeight;
-        }
-
-        const getPositions = ({contextMenu, x, y}) => {
-            let result = {contextMenu, x, y};
-            let maxRight = config.windowInnerWidth - config.contextMenuWidth;
-            let maxBottom = config.windowInnerHeight - contextMenu.offsetHeight;
-
-            if(maxRight < x) {
-                result.x = x - config.contextMenuWidth;
+    const navJson = {
+        "menu" : [
+            {
+                "title" : "Меню 1",
+                "href" : "www.google.com",
+                "menu" : [
+                    {
+                        "title" : "Меню_2 1",
+                        "href" : "www.google.com",
+                        "menu" : [
+                        ]
+                    },
+                    {
+                        "title" : "Меню_2 2",
+                        "href" : "www.google.com",
+                        "menu" : [
+                        ]
+                    },
+                    {
+                        "title" : "Меню_2 3",
+                        "href" : "www.google.com",
+                        "menu" : [
+                            {
+                                "title" : "Меню_3 1",
+                                "href" : "www.google.com",
+                                "menu" : [
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "title" : "Меню 2",
+                "href" : "www.google.com",
+                "menu" : [
+                ]
+            },
+            {
+                "title" : "Меню 3",
+                "href" : "www.google.com",
+                "menu" : [
+                ]
+            },
+            {
+                "title" : "Меню 4",
+                "href" : "www.google.com",
+                "menu" : [
+                    {
+                        "title" : "Меню_4 1",
+                        "href" : "www.google.com",
+                        "menu" : [
+                        ]
+                    },
+                    {
+                        "title" : "Меню_4 2",
+                        "href" : "www.google.com",
+                        "menu" : [
+                            {
+                                "title" : "Меню_4_2 1",
+                                "href" : "www.google.com",
+                                "menu" : [
+                                ]
+                            },
+                            {
+                                "title" : "Меню_4_2 2",
+                                "href" : "www.google.com",
+                                "menu" : [
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "title" : "Меню_4 3",
+                        "href" : "www.google.com",
+                        "menu" : [
+                            {
+                                "title" : "Меню_4_3 1",
+                                "href" : "www.google.com",
+                                "menu" : [
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
+        ]
+    };
 
-            if(maxBottom < y) {
-                result.y = y - contextMenu.offsetHeight;
-            }
-
-            return result;
-        }
-
-        const getSubPositions = ({contextMenu, x, y}) => {
-            let result = {contextMenu, x, y};
-            let maxRight = config.windowInnerWidth - (config.contextMenuWidth * 2);
-            let maxBottom = config.windowInnerHeight - contextMenu.offsetHeight;
-
-            if(maxRight < x) {
-                result.x = 0 + -config.contextMenuWidth;
-            } else {
-                result.x = config.contextMenuWidth - 2;
-            }
-
-            if(maxBottom < y) {
-                result.y = -contextMenu.offsetHeight + 32;
-            } else {
-                result.y = 0;
-            }
-
-            return result;
-        }
-
-        const clearUlShow = () => {
-            [...contextMenu.querySelectorAll('.show')].forEach(item => {
-                item.classList.remove('show');
-            });
-        }
-
-        getWindowInners();
-        clearUlShow();
-
-        if(document.querySelector('.js-context-menu')) {
-            document.querySelector('.js-context-menu').remove();
-        }
-
-        contextMenu.innerHTML = nav;
-
-        e.preventDefault();
-
-        body.insertAdjacentElement('afterbegin', contextMenu);
-
-        const x = e.clientX; // получаем координату X мыши
-        const y = e.clientY; // получаем координату Y мыши
-
-        const contextMenuContainer = getPositions({contextMenu, x, y});
-
-        contextMenu.style.top = `${contextMenuContainer.y}px`;
-        contextMenu.style.left = `${contextMenuContainer.x}px`;
-
-        contextMenu.addEventListener('mouseover', (e) => {
-
-            if(e.target.classList.contains('js-context-menu__link') && e.target.closest('li') && e.target.closest('li').querySelector('ul')) {
-
-                clearUlShow();
-
-                const subMenu = e.target.closest('li').querySelector('ul');
-
-                subMenu.classList.add('show');
-
-                let coord = e.target.getBoundingClientRect();
-
-                const contextMenuContainer = getSubPositions({contextMenu: subMenu, x: coord.left, y: coord.top});
-
-                subMenu.style.top = `${contextMenuContainer.y}px`;
-                subMenu.style.left = `${contextMenuContainer.x}px`;
-
-            }
-        }, false);
-
-        body.addEventListener('click', (eBody) => {
-            contextMenu.remove();
-        });
-
-
-    })
-
+    ContextMenu({
+        selectorElement: '#contextBlock',
+        width: 150,
+        nav: navJson,
+        formatNav: 'JSON' // JSON || HTML; default: HTML;
+    });
 
 });
